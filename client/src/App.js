b1f31c5e146f237920a7345a57b6bc6d86befcb6
samuVillegas <villegassamuel25@gmail.com>
2021-05-06 17:38:36 -0500

    Preparación de carpetas para el desarrollo del frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from '../src/pages/Home';
+
 function App() {
-  return (
-    <div className="App">
+  return(
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home" component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/base/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../../styles/Navbar.css';
+const Navbar = () => {
+    return(
+        <h1>Holi</h1>
+    );
+}
+
+export default Navbar; 
