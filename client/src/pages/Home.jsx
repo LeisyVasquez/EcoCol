6918d3ebb090fdfefca83dfd5b460338d0c0f4a1
SebasRpox <sebaxrpo@gmail.com>
2021-05-26 03:45:33 -0500

    Diseño ruleta listo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-//import LogInNavbar from '../components/base/LogInNavbar';
-//import TravelerNavbar from '../components/base/TravelerNavbar'; 
+import LogInNavbar from '../components/base/LogInNavbar';
+import TravelerNavbar from '../components/base/TravelerNavbar'; 
 import EditorNavbar from '../components/base/EditorNavbar'; 
 import Home from '../components/Home';
 
 const home = () => {
  return(
     <div>
-      <EditorNavbar />
+      <LogInNavbar />
       <Home />
     </div>
  );
