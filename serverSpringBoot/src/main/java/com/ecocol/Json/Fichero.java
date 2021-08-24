5f344c891b0a411e50b31f7b7160b218135d0027
LeisyVasquez <Leisyyvalentina@gmail.com>
2021-08-24 13:03:00 -0500

    Se cambia una ruta del servidor de Java

--- serverSpringBoot/src/main/java/com/ecocol/Json/Fichero.java
+++ serverSpringBoot/src/main/java/com/ecocol/Json/Fichero.java
@@ -9,32 +9,23 @@ import java.io.IOException;
 public class Fichero {
     public static void main(String[] args) {
         String fichero = "";
-
-        try(
-        BufferedReader br = new BufferedReader(new FileReader("place.json")))
-        {
+        String pathPlaceJson = "C:/Users/Leisy/Documents/Poli/PPI/EcoCol/serverSpringBoot/src/main/java/com/ecocol/Json/place.json"; 
+        try (BufferedReader br = new BufferedReader(new FileReader(pathPlaceJson))) {
             String linea;
             while ((linea = br.readLine()) != null) {
                 fichero += linea;
             }
-    
-        }catch(
-        FileNotFoundException ex)
-        {
+
+        } catch (FileNotFoundException ex) {
             System.out.println(ex.getMessage());
-        }catch(
-        IOException ex)
-        {
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-
-        
 
         Gson gson = new Gson();
         Place place = gson.fromJson(fichero, Place.class);
         System.out.println(place);
 
     }
-    
 
 }
